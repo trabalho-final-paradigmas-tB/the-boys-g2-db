23c5ff0fdc4ff655567aa6a6245ad10bd3f4822e
arthurcavalcante <arthur.cavalcante.neves.official@gmail.com>
2024-10-30 20:41:11 -0300

    atualização dos herois

---
+++ sql_database/about_hero.sql
@@ -0,0 +1,33 @@
+-- Tabela dos heróis (Sexo: F/M)
+
+CREATE TABLE herois (
+    CODIGO_HEROI        INT,
+    NOME_REAL           VARCHAR(80)     NULL,
+    NOME_HEROI          VARCHAR(80)     NOT NULL,
+    SEXO                VARCHAR(1)      NOT NULL,
+    ALTURA_HEROI        FLOAT           NOT NULL,
+    PESO_HEROI          FLOAT           NOT NULL,
+    DATA_NASCIMENTO     DATE            NOT NULL,
+    LOCAL_NASCIMENTO    VARCHAR(80)     NULL,
+    PODERES             VARCHAR(80)     NOT NULL,
+    NIVEL_FORCA         INT             NOT NULL,
+    POPULARIDADE        INT             NOT NULL,
+    STATUS              VARCHAR(7)      NOT NULL,
+    HISTORICO_BATALHAS  VARCHAR(80)     NOT NULL
+);
+
+ALTER TABLE herois
+CONSTRAINT PK_CODIGO PRIMARY KEY (CODIGO_HEROI);
+
+ALTER TABLE herois
+AUTO_INCREMENT=100;
+
+CREATE TRIGGER banir_heroi
+BEFORE INSERT OR UPDATE ON herois
+FOR EACH ROW
+BEGIN
+    IF NEW.POPULARIDADE <= 20 THEN
+        SET NEW.STATUS = 'BANIDO';
+    END IF;
+END;
+
