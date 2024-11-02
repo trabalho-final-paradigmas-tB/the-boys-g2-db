9e2e304fa3ae6ef21f97abd762f96bf6f5b73459
Sanderson1406 <sandersonoficial10@hotmail.com>
2024-11-02 13:37:53 -0300

    Alternando as funções de consulta e inserção ao bd

--- handlers/heroController.go
+++ handlers/heroController.go
@@ -11,11 +11,12 @@ import (
 func InserirHeroi(w http.ResponseWriter, r *http.Request) {
 	var heroi models.Heroi
 	if err := json.NewDecoder(r.Body).Decode(&heroi); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, "Erro ao decodificar JSON: "+err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	insertQuery := `INSERT INTO herois (NOME_REAL, NOME_HEROI, SEXO, ALTURA_HEROI, PESO_HEROI, DATA_NASCIMENTO, LOCAL_NASCIMENTO, PODERES, NIVEL_FORCA, POPULARIDADE, STATUS, HISTORICO_BATALHAS) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+
 	res, err := database.DB.Exec(insertQuery,
 		heroi.NomeReal,
 		heroi.NomeHeroi,
@@ -31,16 +32,36 @@ func InserirHeroi(w http.ResponseWriter, r *http.Request) {
 		heroi.HistoricoBatalhas)
 
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Erro ao inserir herói: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	lastInsertID, err := res.LastInsertId()
+	lastInsertID, _ := res.LastInsertId()
+	fmt.Fprintf(w, "Herói inserido com sucesso! Último ID inserido: %d\n", lastInsertID)
+}
+
+func ListarHerois(w http.ResponseWriter, r *http.Request) {
+	query := "SELECT NOME_REAL, NOME_HEROI, SEXO, ALTURA_HEROI, PESO_HEROI, DATA_NASCIMENTO, LOCAL_NASCIMENTO, PODERES, NIVEL_FORCA, POPULARIDADE, STATUS, HISTORICO_BATALHAS FROM herois"
+
+	rows, err := database.DB.Query(query)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Erro ao consultar heróis: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Fprintf(w, "Herói inserido com sucesso! Último ID inserido: %d\n", lastInsertID)
+	defer rows.Close()
+
+	var herois []models.Heroi
+	for rows.Next() {
+		var heroi models.Heroi
+		if err := rows.Scan(&heroi.NomeReal, &heroi.NomeHeroi, &heroi.Sexo, &heroi.AlturaHeroi, &heroi.PesoHeroi, &heroi.DataNascimento, &heroi.LocalNascimento, &heroi.Poderes, &heroi.NivelForca, &heroi.Popularidade, &heroi.Status, &heroi.HistoricoBatalhas); err != nil {
+			http.Error(w, "Erro ao escanear herói: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		herois = append(herois, heroi)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(herois)
 }
 
 /*
