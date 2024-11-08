cd83486d604878478b26da0c6bbb0f309340b048
brunolmars <bruno.lmarques@sempreceub.com>
2024-11-08 20:43:45 -0300

    continuação do missao controller

--- handlers/missoesController.go
+++ handlers/missoesController.go
@@ -8,22 +8,105 @@ import (
 )
 
 func inserirMissao(w http.ResponseWriter, r *http.Request) {
-	var missões models.Missoes
-	err := json.NewDecoder(r.Body).Decode(&missões)
+
+	if database.Db == nil {
+		http.Error(w, "Erro ao conectar ao banco de dados", http.StatusInternalServerError)
+		return
+	}
+
+	var missoes models.Missoes
+	err := json.NewDecoder(r.Body).Decode(&missoes)
 	if err != nil {
 		http.Error(w, "Erro ao ler dados", http.StatusBadRequest)
+		return
+	}
+
+	var rank_escolhida string
+
+	switch missoes.Rank_Escolhida {
+	case "SS":
+		rank_escolhida = missoes.Rank_SS
+	case "S":
+		rank_escolhida = missoes.Rank_S
+	case "A":
+		rank_escolhida = missoes.Rank_A
+	case "B":
+		rank_escolhida = missoes.Rank_B
+	case "C":
+		rank_escolhida = missoes.Rank_C
+	case "D":
+		rank_escolhida = missoes.Rank_D
+	case "E":
+		rank_escolhida = missoes.Rank_E
+	default:
+		http.Error(w, "Rank inválido", http.StatusBadRequest)
+		return
 	}
 
 	query := `
-	INSERT INTO missoes (nome, descricao, classificacao, rank_ss, rank_s, rank_a, rank_b, rank_c, rank_d, rank_e)
-		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id
+	INSERT INTO missoes (nome, descricao, classificacao, rank_escolhido)
+		VALUES ($1, $2, $3, $4) RETURNING id
 	`
 	var id int
 
-	err = database.Db.QueryRow(query, missões.Nome, missões.Descrição, missões.Classificação, missões.Rank_SS, missões.Rank_S, missões.Rank_A, missões.Rank_B, missões.Rank_C, missões.Rank_D, missões.Rank_E).Scan(id)
+	err = database.Db.QueryRow(
+		query,
+		missoes.Nome,
+		missoes.Descrição,
+		missoes.Classificação,
+		rank_escolhida,
+	).Scan(&id)
+
 	if err != nil {
 		http.Error(w, "Erro ao inserir missão", http.StatusInternalServerError)
-
+		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
+	response := map[string]interface{}{
+		"mensagem":       "Missão inserida com sucesso",
+		"status":         "sucesso",
+		"missao":         missoes.Nome,
+		"descrição":      missoes.Descrição,
+		"rank_escolhido": rank_escolhida,
+	}
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "Erro ao enviar a resposta", http.StatusInternalServerError)
+	}
+}
+
+func ListadeMissões(w http.ResponseWriter, r *http.Request) {
+
+	if database.Db == nil {
+		http.Error(w, "Erro de conexão com o banco de dados", http.StatusInternalServerError)
+		return
+	}
+
+	rows, err := database.Db.Query("SELECT nome, descricao, classificacao, rank_escolhido FROM missoes")
+	if err != nil {
+		http.Error(w, "Erro ao listar missões: ", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var missoes []models.Missoes
+
+	for rows.Next() {
+		var missao models.Missoes
+		err := rows.Scan(&missao.Nome, &missao.Descrição, &missao.Classificação, &missao.Rank_Escolhida)
+		if err != nil {
+			http.Error(w, "Erro ao escanear missão: ", http.StatusInternalServerError)
+			return
+		}
+		missoes = append(missoes, missao)
+	}
+
+	if err = rows.Err(); err != nil {
+		http.Error(w, "Erro na leitura das linhas: ", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(missoes)
 }
--- models/missoes.go
+++ models/missoes.go
@@ -1,14 +1,15 @@
 package models
 
 type Missoes struct {
-	Nome          string `json:"Nome da missão"`
-	Descrição     string `json:"Descrição da Missão"`
-	Classificação string `json:"Classificação"`
-	Rank_SS       string `json:"Rank_SS"`
-	Rank_S        string `json:"Rank_S"`
-	Rank_A        string `json:"Rank_A"`
-	Rank_B        string `json:"Rank_B"`
-	Rank_C        string `json:"Rank_C"`
-	Rank_D        string `json:"Rank_D"`
-	Rank_E        string `json:"Rank_E"`
+	Nome           string `json:"nome da missão"`
+	Descrição      string `json:"descrição da missão"`
+	Classificação  string `json:"classificação"`
+	Rank_Escolhida string `json:"rank_escolhido"`
+	Rank_SS        string `json:"rank_SS"`
+	Rank_S         string `json:"rank_S"`
+	Rank_A         string `json:"rank_A"`
+	Rank_B         string `json:"rank_B"`
+	Rank_C         string `json:"rank_C"`
+	Rank_D         string `json:"rank_D"`
+	Rank_E         string `json:"rank_E"`
 }
