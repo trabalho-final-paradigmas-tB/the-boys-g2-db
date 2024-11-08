c4a999dc11b626ea23b93ba0ba75d4812334a6ea
Pedro Luiz <pedro.la@sempreceub.com>
2024-11-08 03:07:34 -0300

    criando structs

--- models/crimes.go
+++ models/crimes.go
@@ -1,4 +1,9 @@
 package models
 
-type Crimes struct {
+type Crime struct {
+	NomeCrime        string `json:"nome_crime"`
+	Descricao        string `json:"descricao"`
+	DataCrime        string `json:"data_crime"`
+	HeroiResponsavel int    `json:"heroi_responsavel"`
+	Severidade       int    `json:"severidade"`
 }
