ed4907c04e930ef22c728e8c4ec087fb6d161e54
brunolmars <bruno.lmarques@sempreceub.com>
2024-11-13 16:51:53 -0300

    implementação do resulado missão

--- handlers/missoesController.go
+++ handlers/missoesController.go
@@ -193,3 +193,49 @@ func ModificarMissao(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]string{
 		"mensagem": "Missão atualizada com sucesso!"})
 }
+
+func Resultadomissão(w http.ResponseWriter, r *http.Request) {
+	if database.Db == nil {
+		http.Error(w, "Erro ao conectar banco de dados", http.StatusInternalServerError)
+		return
+	}
+	var missao models.Missoes
+	var herois models.Heroi
+
+	err := json.NewDecoder(r.Body).Decode(&missao)
+	if err != nil {
+		http.Error(w, "Erro ao ler dados da missão", http.StatusBadRequest)
+		return
+	}
+
+	if missao.Dificuldade < 1 || missao.Dificuldade > 10 {
+		http.Error(w, "Nivel de dificuldade invalido", http.StatusBadRequest)
+		return
+	}
+	if herois.NivelForca < 1 || herois.NivelForca > 10 {
+		http.Error(w, "Nivel de força invalido", http.StatusBadRequest)
+		return
+	}
+
+	if missao.Dificuldade > herois.NivelForca {
+		w.Header().Set("Content-Type", "application/json")
+		response := map[string]interface{}{
+			"mensagem": "Missão foi um Fracasso",
+		}
+		if err := json.NewEncoder(w).Encode(response); err != nil {
+			http.Error(w, "Erro ao enviar a resposta", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	if missao.Dificuldade <= herois.NivelForca {
+		w.Header().Set("Content-Type", "application/json")
+		response := map[string]interface{}{
+			"mensagem": "Missão concluída com sucesso",
+		}
+		if err := json.NewEncoder(w).Encode(response); err != nil {
+			http.Error(w, "Erro ao enviar a resposta", http.StatusInternalServerError)
+		}
+	}
+
+}
